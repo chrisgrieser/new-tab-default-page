ca9220f08770f8c682af072c1b2d7a2242a55649
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-10-01 01:18:56 +0200

    style: biome formatting

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,21 @@ declare module "obsidian" {
 	interface App {
 		internalPlugins: {
 			plugins: {
-				switcher: { enabled: boolean }
+				switcher: { enabled: boolean };
 			};
 		};
 		commands: {
 			executeCommandById: (commandID: string) => boolean;
-		}
-
+		};
 	}
 }
 
 // Default Settings
 interface DefaultNewTabPageSettings {
-	whatToOpen: string,
-	filePath: string,
-	mode: string,
-	compatibilityMode: boolean,
+	whatToOpen: string;
+	filePath: string;
+	mode: string;
+	compatibilityMode: boolean;
 }
 
 const DEFAULT_SETTINGS: Partial<DefaultNewTabPageSettings> = {
@@ -31,25 +30,31 @@ const DEFAULT_SETTINGS: Partial<DefaultNewTabPageSettings> = {
 	compatibilityMode: false,
 };
 
+//──────────────────────────────────────────────────────────────────────────────
+
 export default class defaultNewTabPage extends Plugin {
 	settings: DefaultNewTabPageSettings;
 
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new DefaultNewTabPageSettingTab(this.app, this));
-		const { workspace } = this.app
+		const { workspace } = this.app;
 
 		workspace.onLayoutReady(() => {
 			// Get existing leaves (tabs)
 			const existingLeaves = new WeakSet<WorkspaceLeaf>();
-			workspace.iterateAllLeaves(leaf => { existingLeaves.add(leaf) });
+			workspace.iterateAllLeaves((leaf) => {
+				existingLeaves.add(leaf);
+			});
 
 			// When layout changes, check for new ones
 			// second arg needs to be arrow-function, so that `this` gets set
 			// correctly. https://discord.com/channels/686053708261228577/840286264964022302/1016341061641183282
-			this.registerEvent(workspace.on("layout-change", () => {
-				this.checkForNewTab(existingLeaves);
-			}));
+			this.registerEvent(
+				workspace.on("layout-change", () => {
+					this.checkForNewTab(existingLeaves);
+				}),
+			);
 		});
 
 		console.debug("New Tab Default Page Plugin loaded.");
@@ -63,10 +68,13 @@ export default class defaultNewTabPage extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	async onunload() { console.debug("New Tab Default Page Plugin unloaded.") }
+	async onunload() {
+		console.debug("New Tab Default Page Plugin unloaded.");
+	}
 
-	checkForNewTab (existingLeaves: WeakSet<WorkspaceLeaf>) { // WeakSet won't hold references, unused leaves are automatically removed
-		this.app.workspace.iterateAllLeaves(leaf => {
+	checkForNewTab(existingLeaves: WeakSet<WorkspaceLeaf>) {
+		// WeakSet won't hold references, unused leaves are automatically removed
+		this.app.workspace.iterateAllLeaves((leaf) => {
 			if (existingLeaves.has(leaf)) return; // only ever check a new leaf once
 			existingLeaves.add(leaf);
 
@@ -77,73 +85,67 @@ export default class defaultNewTabPage extends Plugin {
 		});
 	}
 
-	runCommand (commandId: string, leaf: WorkspaceLeaf) {
+	runCommand(commandId: string, leaf: WorkspaceLeaf) {
 		const delay = commandId.includes("switcher") ? 200 : 0; // eslint-disable-line no-magic-numbers
 		setTimeout(() => {
 			if (!this.tabIsEmpty(leaf)) return;
 			const commandExists = this.app.commands.executeCommandById(commandId);
 			const success = commandExists !== false; // INFO on success, commandExists is undefined, otherwise false
-			if (!success) new Notice ("Plugin for the New Tab Page is not enabled.");
+			if (!success) new Notice("Plugin for the New Tab Page is not enabled.");
 		}, delay);
 	}
 
-	tabIsEmpty (leaf: WorkspaceLeaf) {
+	tabIsEmpty(leaf: WorkspaceLeaf) {
 		return !leaf.view || leaf.view.getViewType() === "empty";
 	}
 
-	// In compatibility mode, we have to check if the view state type of a leaf is
+	// INFO In compatibility mode, we have to check if the view state type of a leaf is
 	// still empty at the next tick. Other plugins might open a new leaf and change the view state
 	// after opening the leaf. The layout-change listener is triggered before the state
 	// might be changed. This might cause this.checkForNewTab to conclude that the view state
 	// is empty, whilst in fact is only STILL empty but will be overwritten.
 	// An example of how other plugins might open leafs and change the view state:
 	// https://github.com/marcusolsson/obsidian-projects/blob/1.16.3/src/main.ts#L207
-	async leafIsStillEmpty (leaf: WorkspaceLeaf) {
-		return new Promise((resolve, reject) => {
+	async leafIsStillEmpty(leaf: WorkspaceLeaf) {
+		return new Promise((resolve, _reject) => {
 			process.nextTick(() => {
-				const viewStateAfterDelay = leaf.getViewState()
-				resolve((viewStateAfterDelay as any)?.type === 'empty')
-			})
+				const viewStateAfterDelay = leaf.getViewState();
+				resolve(viewStateAfterDelay?.type === "empty");
+			});
 		});
 	}
 
-	async openDefaultPage (leaf: WorkspaceLeaf) {
+	async openDefaultPage(leaf: WorkspaceLeaf) {
 		const newTabPage = this.settings.filePath;
 		if (!newTabPage) return;
 
 		const tFiletoOpen = this.app.metadataCache.getFirstLinkpathDest(newTabPage, "/"); // `getFirstLinkpathDest` more reliably finds match than `getAbstractFileByPath`, e.g. with missing file extensions
 		const pathIsValid = Boolean(tFiletoOpen);
 		if (!pathIsValid) {
-			new Notice (`${newTabPage} is not a valid path to a note in your vault.`);
+			new Notice(`${newTabPage} is not a valid path to a note in your vault.`);
 			return;
 		}
 
-		
-		if(this.settings.compatibilityMode){
-			// When compatibility mode is enabled, check if leaf is still empty
-			if(!await this.leafIsStillEmpty(leaf)) return;
+		// When compatibility mode is enabled, check if leaf is still empty
+		if (this.settings.compatibilityMode) {
+			if (!(await this.leafIsStillEmpty(leaf))) return;
 		}
-		
+
 		await leaf.openFile(tFiletoOpen);
 		this.setViewMode(leaf, this.settings.mode);
 	}
 
-	setViewMode (leaf: WorkspaceLeaf, targetMode: string) {
+	setViewMode(leaf: WorkspaceLeaf, targetMode: string) {
 		const view = leaf.getViewState();
-		switch (targetMode) {
-			case "source-mode":
-				view.state.mode = "source";
-				view.state.source = true;
-				break;
-			case "live-preview":
-				view.state.mode = "source";
-				view.state.source = false;
-				break;
-			case "reading-mode":
-				view.state.mode = "preview";
-				break;
+		if (targetMode === "source-mode") {
+			view.state.mode = "source";
+			view.state.source = true;
+		} else if (targetMode === "live-preview") {
+			view.state.mode = "source";
+			view.state.source = false;
+		} else if (targetMode === "reading-mode") {
+			view.state.mode = "preview";
 		}
 		leaf.setViewState(view);
 	}
-
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-// https://marcus.se.net/obsidian-plugin-docs/user-interface/settings#register-a-settings-tab
+// DOCS https://marcus.se.net/obsidian-plugin-docs/user-interface/settings#register-a-settings-tab
 import { App, PluginSettingTab, Setting } from "obsidian";
 import defaultNewTabPage from "./main";
 
@@ -17,52 +17,58 @@ export class DefaultNewTabPageSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName("New Tab opens…")
-			.setDesc("What to open when a new tab is created. (Except for the new tab page, the respective plugin needs to be enabled.)")
-			.addDropdown(dropdown => { dropdown
-				// INFO: except for the new tab page, the values should be equal to
-				// the command-id to run
-				.addOptions({
-					"new-tab-page": "New Tab Page",
-					"daily-notes": "Daily Note (Core Plugin)",
-					"periodic-notes:open-daily-note": "Daily Note (Periodic Notes Plugin)",
-					"periodic-notes:open-weekly-note": "Weekly Note (Periodic Notes Plugin)",
-					"periodic-notes:open-monthly-note": "Monthly Note (Periodic Notes Plugin)",
-					"random-note": "Random Note (Core Plugin)",
-					"switcher:open": "Quick Switcher (Core Plugin)",
-					"obsidian-another-quick-switcher:search-command_recent-search": "Another Quick Switcher",
-					"darlal-switcher-plus:switcher-plus:open": "Quick Switcher++",
-				})
-				.setValue(this.plugin.settings.whatToOpen)
-				.onChange(async (value) => {
-					this.plugin.settings.whatToOpen = value;
-					await this.plugin.saveSettings();
-				});
+			.setDesc(
+				"What to open when a new tab is created. (Except for the new tab page, the respective plugin needs to be enabled.)",
+			)
+			.addDropdown((dropdown) => {
+				dropdown
+					// INFO: except for the new tab page, the values should be equal to
+					// the command-id to run
+					.addOptions({
+						"new-tab-page": "New Tab Page",
+						"daily-notes": "Daily Note (Core Plugin)",
+						"periodic-notes:open-daily-note": "Daily Note (Periodic Notes Plugin)",
+						"periodic-notes:open-weekly-note": "Weekly Note (Periodic Notes Plugin)",
+						"periodic-notes:open-monthly-note": "Monthly Note (Periodic Notes Plugin)",
+						"random-note": "Random Note (Core Plugin)",
+						"switcher:open": "Quick Switcher (Core Plugin)",
+						"obsidian-another-quick-switcher:search-command_recent-search": "Another Quick Switcher",
+						"darlal-switcher-plus:switcher-plus:open": "Quick Switcher++",
+					})
+					.setValue(this.plugin.settings.whatToOpen)
+					.onChange(async (value) => {
+						this.plugin.settings.whatToOpen = value;
+						await this.plugin.saveSettings();
+					});
 			});
 
 		new Setting(containerEl)
 			.setName("Default New Tab Page")
-			.setDesc("Path of the note that will be opened in new tabs. Images and PDFs also work. Only takes effect when the setting above is 'New Tab Page'.")
-			.addText(text => text
-				.setPlaceholder("Meta/Homepage.md")
-				.setValue(this.plugin.settings.filePath)
-				.onChange(async (value) => {
-					this.plugin.settings.filePath = value;
-					await this.plugin.saveSettings();
-				})
+			.setDesc(
+				"Path of the note that will be opened in new tabs. Images and PDFs also work. Only takes effect when the setting above is 'New Tab Page'.",
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Meta/Homepage.md")
+					.setValue(this.plugin.settings.filePath)
+					.onChange(async (value) => {
+						this.plugin.settings.filePath = value;
+						await this.plugin.saveSettings();
+					}),
 			);
 
 		new Setting(containerEl)
 			.setName("Mode")
 			.setDesc("Select the mode in which the default new tab page will be opened.")
-			.addDropdown(dropdown => {
+			.addDropdown((dropdown) => {
 				dropdown
-				.addOptions({
-					"obsidian-default": "Obsidian Default",
-					"live-preview": "Live Preview",
-					"reading-mode": "Reading Mode",
-					"source-mode": "Source Mode"
-				})
-				.setValue(this.plugin.settings.mode)
+					.addOptions({
+						"obsidian-default": "Obsidian Default",
+						"live-preview": "Live Preview",
+						"reading-mode": "Reading Mode",
+						"source-mode": "Source Mode",
+					})
+					.setValue(this.plugin.settings.mode)
 					.onChange(async (value) => {
 						this.plugin.settings.mode = value;
 						await this.plugin.saveSettings();
@@ -70,15 +76,15 @@ export class DefaultNewTabPageSettingTab extends PluginSettingTab {
 			});
 
 		new Setting(this.containerEl)
-			.setName("Compatability mode")
-			.setDesc("Enable compatability mode for other plugins (e.g. Obsidian Projects) which open new tabs. This introduces minor delays.")
+			.setName("Compatibility mode")
+			.setDesc(
+				"Enable compatibility mode for other plugins (e.g. Obsidian Projects) which open new tabs. This introduces minor delays.",
+			)
 			.addToggle((toggle) => {
-			  toggle
-				 .setValue(this.plugin.settings.compatibilityMode)
-				 .onChange(async (value) => {
+				toggle.setValue(this.plugin.settings.compatibilityMode).onChange(async (value) => {
 					this.plugin.settings.compatibilityMode = value;
 					await this.plugin.saveSettings();
-				})
+				});
 			});
 	}
 }
